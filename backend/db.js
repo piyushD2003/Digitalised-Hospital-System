e0aa1a877fc3804ab04bc4ba2c1176596f9650c9
Piyush Dhyani <piyushdhyani7@gmail.com>
2024-05-04 23:05:13 +0530

    change the url of mongodb from local to atlas

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/hospitalmanagement'
+const mongoURI = 'mongodb+srv://MongoHMS:dhyanipiyu7@cluster0.v5ntdgt.mongodb.net/'
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI)
